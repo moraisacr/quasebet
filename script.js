49b98fc856782a904dab8c929d0315b8a921a489
Augusto Cesar Rezende de Morais <ast.bio@hotmail.com>
2025-03-09 14:14:07 -0300

    fix

---
+++ script.js
@@ -0,0 +1,24 @@
+function placeBet() {
+    let sport = document.getElementById("sport").value;
+    let team = document.getElementById("team").value;
+    let betAmount = document.getElementById("betAmount").value;
+    let resultText = document.getElementById("result");
+
+    if (!betAmount || betAmount < 10) {
+        resultText.innerHTML = "O valor mínimo da aposta é R$10.";
+        resultText.style.color = "red";
+        return;
+    }
+
+    let winningOutcome = Math.random();
+    let betWon = winningOutcome > 0.5;
+
+    if (betWon) {
+        let winnings = (betAmount * 2).toFixed(2);
+        resultText.innerHTML = `VOCE QUASE GANHOU (${team}) ganhou no ${sport}! VOCE QUASE GANHOU $${winnings}.`;
+        resultText.style.color = "#00ffcc";
+    } else {
+        resultText.innerHTML = `VOCE QUASE GANHOU! O time (${team}) perdeu no ${sport}. Tente novamente!`;
+        resultText.style.color = "#ff0000";
+    }
+}
